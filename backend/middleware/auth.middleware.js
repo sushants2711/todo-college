3234dec6b6d0ed9e492db5c9b0e3a6feb20b7c28
Sushant Singh <sushantsingh@Sushants-MacBook-Air.local>
2025-05-07 13:00:21 +0530

    delete middlewarw

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -57,3 +57,32 @@ export const loginMiddleware = async (req, res, next) => {
         });
     };
 };
+
+export const deleteUserAccountMiddleware = async (req, res, next) => {
+    try {
+        const schema = joi.object({
+            name: joi.string().min(2).max(50).required(),
+            email: joi.string().email().min(10).max(50).trim().required(),
+            password: joi.string().min(8).max(1024).required(),
+        });
+
+        const { error } = schema.validate(req.body);
+
+        if (error) {
+            return res.status(400).json({
+                success: false,
+                message: "Validation failed",
+                error: error.details[0].message
+            });
+        };
+
+        next();
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server error",
+            error: error.message
+        })
+    }
+}
--- backend/routers/auth.route.js
+++ backend/routers/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUserAccount, login, logout, signup } from "../controllers/auth.controller.js";
-import { loginMiddleware, signupMiddleware } from "../middleware/auth.middleware.js";
+import { deleteUserAccountMiddleware, loginMiddleware, signupMiddleware } from "../middleware/auth.middleware.js";
 import { ensureAuthentication } from "../middleware/jwt.cookies.token.verify.js";
 
 const authRouter = express.Router();
@@ -8,6 +8,6 @@ const authRouter = express.Router();
 authRouter.route("/signup").post(signupMiddleware, signup);
 authRouter.route("/login").post(loginMiddleware, login);
 authRouter.route("/logout").post(ensureAuthentication, logout);
-authRouter.route("/delete").post(ensureAuthentication, deleteUserAccount);
+authRouter.route("/delete").post(ensureAuthentication, deleteUserAccountMiddleware, deleteUserAccount);
 
 export default authRouter;
